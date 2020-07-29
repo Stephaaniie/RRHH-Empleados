d32a44d5edb3f1a722e1228d53682463ba40ce08
stephaaniie <stephanie.castillo@gmail.com>
2020-07-29 19:33:48 -0300

    Se modifico la carpeta de los test pero falta agregar mas test unitarios

--- src/test/java/ar/com/ada/api/empleado/empleado/DemoApplicationTests.java
+++ src/test/java/ar/com/ada/api/empleado/empleado/DemoApplicationTests.java
@@ -1,13 +1,8 @@
 package ar.com.ada.api.empleado.empleado;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
